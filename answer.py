2911fc91861d434346bd2b4a2656f5232064a7eb
akramMk <akrammekbal10@gmail.com>
2025-03-16 13:55:32 +0100

    add answer function

---
+++ answer.py
@@ -0,0 +1,17 @@
+
+
+def getAnswer(question, context, model):
+    #Prompt
+    template = "You are an expert in deep learning and computer vision\
+                Your role is to provide searchers with informations related to punlished research papers , \
+                prioritizing the provided semantic search context retrieved from the paper. \
+                Use the context directly if it addresses the researcher's question comprehensively. \
+                Refer to your expertise to augment the explanation only if you think the context is insufficient.\n\
+                Searcher Question: {question}\n\
+                Search Context: {context}"
+
+    augmented_template = template.format(question=question, context=context)
+
+    #generate response
+    answer = model.invoke(augmented_template)
+    return answer.content
