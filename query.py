f5771fcd3fffc7b7937001e5915dd4bcbdb8bcfc
akramMk <akrammekbal10@gmail.com>
2025-03-16 13:45:05 +0100

    add query function

---
+++ query.py
@@ -0,0 +1,14 @@
+from embed import ada_get_embedding
+
+def query(index, query_text):
+    """
+    Query the Pinecone index with the given query text and image.
+    Args:
+        index (_type_): _description_
+        query_text (_type_): _description_
+    """
+    query_embedding = ada_get_embedding(query_text[0])
+    results = index.query(vector=[query_embedding], top_k=5, include_metadata=True)
+    context = [results.matches[i].metadata['Content'] for i in range(len(results.matches))]
+    
+    return context
